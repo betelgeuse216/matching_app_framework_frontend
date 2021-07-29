70d1eece1dbbc5293a54be4a03bcd1807be93647
betelgeuse216 <betelgeuse216@gmail.com>
2021-07-29 22:53:48 +0900

    delete custom page unused.

--- lib/ui/base.dart
+++ lib/ui/base.dart
@@ -103,35 +103,3 @@ class _BasePageState extends State<BasePage> {
     );
   }
 }
-
-class CustomPage extends StatelessWidget {
-  final Color pannelColor;
-  final String title;
-
-  CustomPage({@required this.pannelColor, @required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    final titleTextStyle = Theme.of(context).textTheme.title;
-    return Container(
-      child: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-              color: pannelColor,
-              borderRadius: BorderRadius.all(Radius.circular(20.0))),
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: titleTextStyle.fontSize,
-                color: titleTextStyle.color,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
